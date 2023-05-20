35c737b5c537a315a00e3289453d21cf9d1c6c85
LenaMorianu <lenus_b@yahoo.com>
2023-05-21 00:32:38 +0200

    Update WebPage.py
    PR46

--- WebPage.py
+++ WebPage.py
@@ -71,7 +71,7 @@ tab1.text_input(
 tab2.subheader("Preguntas deporte")
 #placeholder = tab2.empty()
 #tab2.button = placeholder.button('CALCULAR IQ DEPORTE', disable = False, key = 1)
-tab2.boton_calcular_IQ = tab2.button('CALCULAR IQ DEPORTE', disabled = False, key = 1)
+tab2.boton_calcular_IQ = tab2.button('CALCULAR IQ DEPORTE', disabled = False, key = 1, on_click = disable)
 
 if tab2.boton_calcular_IQ:
   IQ = np.random.randint(45, 155)
@@ -79,6 +79,6 @@ if tab2.boton_calcular_IQ:
   tab2.write(IQ)
   tab2.write()
   tab2.balloons()
-  tab2.boton_calcular_IQ = tab2.button(disable = True, key = 1)
+  #tab2.boton_calcular_IQ = tab2.button(disable = True, key = 1)
   #placeholder.button('CALCULAR IQ DEPORTE', disabled=True, key='2')
   #tab2.write(data)
