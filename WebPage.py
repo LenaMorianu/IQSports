0220d00b53443945d32892b0bd4ae84b182d888e
Lena Morianu <lenus_b@yahoo.com>
2023-05-20 22:30:50 +0200

    testing1

---
+++ WebPage.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+st.set_page_config(page_title="IQ_Sports",
+                   #page_icon="🧊",
+                   layout="wide",
+                   initial_sidebar_state="expanded")
+
+st.title('IQ TEST - SPORTS')
+         
+st.write('Title - Try this IQ TEST')
+  
+st.write('')
+st.write('*******************************************')
