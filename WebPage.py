708770626f6e4c6c3cc3c09233a2624aa4e46348
LenaMorianu <lenus_b@yahoo.com>
2023-05-21 00:38:01 +0200

    Update WebPage.py
    PR49

--- WebPage.py
+++ WebPage.py
@@ -72,8 +72,8 @@ tab2.subheader("Preguntas deporte")
 #placeholder = tab2.empty()
 #tab2.button = placeholder.button('CALCULAR IQ DEPORTE', disable = False, key = 1)
 
-disabled=tab2.session_state.get("disabled", True)
-tab2.boton_calcular_IQ = tab2.button('CALCULAR IQ DEPORTE', on_click=disabled)
+#disabled=tab2.session_state.get("disabled", True)
+tab2.boton_calcular_IQ = tab2.button('CALCULAR IQ DEPORTE', disabled=st.session_state.get("disabled", True))
 
 if tab2.boton_calcular_IQ:
   IQ = np.random.randint(45, 155)
