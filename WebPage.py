026044e3bcdb5433c31db018ebf3c46ace7b37ff
LenaMorianu <lenus_b@yahoo.com>
2023-05-21 22:21:04 +0200

    Update WebPage.py
    PR84

--- WebPage.py
+++ WebPage.py
@@ -95,11 +95,11 @@ def disable():
 tab2.boton_calcular_IQ = tab2.button('CALCULAR IQ DEPORTE', key='iq_button', on_click=disable)
 tab2.write()
     
-if 'iq_button' not in st.session_state:
-    st.session_state.iq_button =  np.random.randint(45, 155)
+if 'IQ' not in st.session_state:
+    st.session_state.IQ =  np.random.randint(45, 155)
     
 if 'iq_button':
-  IQ = st.session_state.iq_button
+  IQ = st.session_state.IQ
   tab2.metric(label="IQ", value=IQ)
   #tab2.write(st.session_state.iq_button)  tab2.write()
   tab2.write()
