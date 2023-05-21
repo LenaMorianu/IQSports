c1ce5248a2d1fe4c2ecbbf8338204679c1f10add
LenaMorianu <lenus_b@yahoo.com>
2023-05-21 22:38:52 +0200

    Update WebPage.py
    PR89

--- WebPage.py
+++ WebPage.py
@@ -85,12 +85,17 @@ Respuesta1 = tab2.text_input(
 if Respuesta1:
   st.write("You entered: ", Respuesta1)
 
-boton_calcular_IQ = tab2.button('CALCULAR IQ DEPORTE', key='iq_button')
-tab2.write()
+def disable(button):
+  if button in st.session_state:
+    st.session_state.button = disable
     
+  
+boton_calcular_IQ = tab2.button('CALCULAR IQ DEPORTE', key='iq_button', on_click = disable, args=True)
+tab2.write()
+ 
 if 'IQ' not in st.session_state:
     st.session_state.IQ =  np.random.randint(45, 155)
-    
+
 if boton_calcular_IQ:
   IQ = st.session_state.IQ
   #tab2.metric(label="IQ", value=IQ)
