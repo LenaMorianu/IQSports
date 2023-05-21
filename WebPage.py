f45d9d4f547e938feb22bc512c1487d0ed1603e4
LenaMorianu <lenus_b@yahoo.com>
2023-05-21 09:00:12 +0200

    Update WebPage.py
    PR58

--- WebPage.py
+++ WebPage.py
@@ -70,15 +70,11 @@ tab1.text_input(
     
 tab2.subheader("Preguntas deporte")
 
-def disable():
-    st.session_state.disabled = True
+#def disable():
+ #   st.session_state.disabled = True
 
-def disable(button):
-    st.session_state["disabled"] = button
-    
-tab2.boton_calcular_IQ = tab2.button('CALCULAR IQ DEPORTE', key='iq_button', disabled=st.session_state.get("disabled", True))
+tab2.boton_calcular_IQ = tab2.button('CALCULAR IQ DEPORTE', key='iq_button', disabled=False)
 tab2.write()
-tab2.boton_calcular_IQ_2 = tab2.button('CALCULAR IQ DEPORTE2', key='iq_button2', disabled=st.session_state.get("disabled", True))
 
 if tab2.boton_calcular_IQ:
   IQ = np.random.randint(45, 155)
@@ -88,13 +84,6 @@ if tab2.boton_calcular_IQ:
   tab2.write(IQ)
   tab2.write()
   tab2.balloons()
+  tab2.boton_calcular_IQ = tab2.button('CALCULAR IQ DEPORTE', key='iq_button', disabled=True)
   
-if tab2.boton_calcular_IQ_2:
-  IQ2 = np.random.randint(45, 155)
-  tab2.metric(label="IQ", value=IQ2)
-  tab2.write('')
-  tab2.write("IQ2: ")
-  tab2.write(IQ2)
-  tab2.write()
-  tab2.balloons()
 
