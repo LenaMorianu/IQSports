028c16d95e72597d0008d9dd144e20615100e462
LenaMorianu <lenus_b@yahoo.com>
2023-05-21 09:52:57 +0200

    Update WebPage.py
    PR73

--- WebPage.py
+++ WebPage.py
@@ -81,7 +81,7 @@ tab1.text_input(
     #if text_input:
     #   st.write("You entered: ", text_input)
     
- tab2.subheader("Preguntas deporte")
+tab2.subheader("Preguntas deporte")
 
 tab2.boton_calcular_IQ = tab2.button('CALCULAR IQ DEPORTE', key='iq_button')
 tab2.write()
