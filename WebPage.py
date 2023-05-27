5ce0ccbbc771d419f0b1ee4988d0c5e4c4c74834
LenaMorianu <lenus_b@yahoo.com>
2023-05-27 13:54:35 +0200

    Update WebPage.py
    5

--- WebPage.py
+++ WebPage.py
@@ -131,7 +131,7 @@ if Respuesta3:
   st.write("Respuesta : ", Respuesta3)   
     
 df2 = pd.DataFrame([Nombre, Edad, Deporte_favorito, Horas_deporte, Respuesta1, Respuesta2, Respuesta3])
-df.append(df2)
+df.append(df2, ignore_index=True)
 
 df.to_csv('Data.csv', mode='a', index=False, header=False, sep=',')
     
