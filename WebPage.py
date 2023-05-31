721a4c748c4a08524c3c2fb1ab9cd77cdde9c889
LenaMorianu <lenus_b@yahoo.com>
2023-05-31 22:12:25 +0200

    Update WebPage.py
    9969

--- WebPage.py
+++ WebPage.py
@@ -7,7 +7,7 @@ from google.oauth2 import service_account
 
 # Create a connection object.
 credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"],
+    st.secrets["service_account"],
     scopes=[
         "https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive"
     ],
