1078d757e933a3c5844218b2ce509a976dea0010
LenaMorianu <lenus_b@yahoo.com>
2023-05-31 22:54:42 +0200

    Update WebPage.py
    852

--- WebPage.py
+++ WebPage.py
@@ -228,11 +228,11 @@ if boton_calcular_IQ:
   
 
 df = pd.DataFrame(
-  np.random.randn(155, 3),
-  columns=['a', 'b', 'c'])
+  np.random.randn(45, 155),
+  columns=['a', 'b'])
 
 c = alt.Chart(df).mark_circle().encode(
-    x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+    x='a', y='b', size='c', tooltip=['a', 'b'])
 
 st.write(c)
   
